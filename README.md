22789c484213f64f40c575d54d3927b12df76d61
Bogdan Hepting[alias] <bogdan.hepting@lemberg.co.uk>
2017-08-03 12:13:03 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,91 @@
-# icomoon-watcher
+# gulp-icomoon-builder
+This module is used for automate Icomoon integration with your SCSS project.
+
+### Instalation
+`npm install gulp-icomoon-builder`
+
+### Usage
+You can use this module in two ways:
+ * Add icons to SCSS file as [variables](http://sass-lang.com/guide#topic-2) or [variable maps](http://blog.grayghostvisuals.com/sass/real-sass-real-maps/)
+ * Build [SVG sprites](https://css-tricks.com/svg-sprites-use-better-icon-fonts/) and insert icon like this `<svg><use xlink:href="#icon-my-awesome-icon"></use></svg>`
+ 
+**SCSS way:**
+
+You can create gulp task and add it to watcher. Task example:
+ 
+```
+gulp.task('build-fonts', () => {
+  gulp.src('./path_to_font/icomoon/selection.json')
+    .pipe(icomoon({
+      templateType: 'map',
+      extraIcons: {
+        'custom-icon-1': '\f2b9',
+        'custom-icon-2': '\f2ba',
+      },
+    }))
+    .on('error', function (error) {
+      console.log(error);
+      // or if you use "gulp-notify"
+      notify().write(error);
+    })
+
+    .pipe(gulp.dest('scss'))
+    .on('error', function (error) {
+      console.log(error)
+      notify().write(error);
+    });
+});
+```
+**SVG Sprite way:**
+
+**Note:** Parameter `extraIcons` not working for svg sprites
+
+```
+gulp.task('build-svg-sprite', () => {
+  gulp.src('./path_to_font/icomoon/selection.json')
+    .pipe(icomoon({
+      templateType: 'svg',
+      filename: 'icons.svg',
+    }))
+    .on('error', function (error) {
+      console.log(error);
+      // or if you use "gulp-notify"
+      notify().write(error);
+    })
+
+    .pipe(gulp.dest('icons'))
+    .on('error', function (error) {
+      console.log(error)
+      notify().write(error);
+    });
+});
+```
+
+### Options
+
+Option | Type | Default | Description
+--- | --- | --- | ---
+Option | Type | Default | Description
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,125 @@
+'use strict';
+
+const through = require('through2');
+const merge = require('merge');
+const gutil = require('gulp-util');
+const mustache = require('mustache');
+const fs = require('fs');
+
+const plugin = {
+  name: 'gulp-icomoon-parser',
+  error: gutil.PluginError,
+};
+
+module.exports = function (opts) {
+  return through.obj(function (file, enc, cb) {
+    if (file.isNull()) {
+      return cb(new plugin.error(plugin.name, 'File ' + file + ' cannot be found'));
+    }
+    if (file.isStream()) {
+      return cb(new plugin.error(plugin.name, 'Streaming is not supported'));
+    }
+
+    let icomoon, template,
+      renderedTemplate,
+      options = merge({
+        templateType: 'map', // var, map, svg?
+        extraIcons: {},
+        externalTemplare: false,
+        filename: '_icons.scss',
+      }, opts);
+    let mustacheVars = {};
+    mustacheVars.icons = [];
+    mustacheVars.extraIcons = [];
+
+    // Load icomoon
+    try {
+      icomoon = JSON.parse(file.contents.toString(enc));
+    } catch (e) {
+      return cb(new plugin.error(plugin.name, 'Failed to parse IcoMoon project file: ' + e.message));
+    }
+
+    if (options.templateType === 'svg') {
+      // Add icons path to mustache array to form svg
+      icomoon.icons.forEach(function (icon) {
+        mustacheVars.icons.push({
+          path: icon.icon.paths,
+          grid: icon.icon.grid,
+          name: icon.properties.name,
+        });
+      });
+
+    } else {
+      // Add icons to mustache array
+      icomoon.icons.forEach(function (icon) {
+        let iconProperties = icon.properties;
+
+        mustacheVars.icons.push({
+          name: iconProperties.name,
+          code: iconProperties.code.toString(16),
+        })
+      });
+
+      // Add extra icons to mustache array
+      if (typeof options.extraIcons === 'object') {
+        for (let i in options.extraIcons) {
+          if (options.extraIcons.hasOwnProperty(i)) {
+            let name = i, code = options.extraIcons[i];
+
+            if (typeof code === 'string') {
+              code = code.replace("\\", "");
+            } else if (typeof code === 'number') {
+              code = code.toString(16);
+            } else {
+              return cb(new plugin.error(plugin.name, 'The "value" of the  parameter should be string or number'));
+            }
+
+            mustacheVars.extraIcons.push({
+              name: name,
+              code: code,
+            })
+          }
+        }
+
+      } else {
+        return cb(new plugin.error(plugin.name, 'Parameter "extraIcons" should be Object - {"key": "value"}'));
+      }
+    }
+
+    // Set template
+    if (typeof options.templateType === 'string') {
+      let tpl = options.templateType;
+      let src = __dirname + '/templates/icomoon.' + tpl + '.mustache';
+
+      if (typeof options.externalTemplare === 'string') {
+        src = options.externalTemplare;
+      }
+
+      fs.readFile(src, 'utf8', function (error, data) {
+        if (error) {
+          return cb(new plugin.error(plugin.name, 'Failed to read template file, ' + error));
+        }
+        template = data;
+
+        try {
+          renderedTemplate = mustache.render(template, mustacheVars);
+        } catch (e) {
+          return cb(new plugin.error(plugin.name, 'Failed to compile template'));
+        }
+
+        // Pass file to Gulp processing
+        cb(null, new gutil.File({
+          cwd: file.cwd,
+          base: file.base,
+          path: file.base + options.filename,
+          contents: new Buffer(renderedTemplate)
+        }));
+      });
+
+    } else {
+      return cb(new plugin.error(plugin.name, 'Parameter "templateType" should be string.'));
+    }
+
+
+  });
+};
